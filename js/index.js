3a25a5f61e9515c4a1c50d6a2178784069032806
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2023-07-18 16:59:41 +0400

    change time

--- js/index.js
+++ js/index.js
@@ -1,45 +1,41 @@
-`use strict`
+`use strict`;
 
-let second_elements = document.querySelectorAll(".seconds"); 
-let minute_elements = document.querySelectorAll(".minutes"); 
-let hour_elements = document.querySelectorAll(".hours"); 
-let day_elements = document.querySelectorAll(".days"); 
+let second_elements = document.querySelectorAll(".seconds");
+let minute_elements = document.querySelectorAll(".minutes");
+let hour_elements = document.querySelectorAll(".hours");
+let day_elements = document.querySelectorAll(".days");
 
 let countdown = () => {
-    let countDate = new Date("May 7, 2023 00:00:00").getTime();
-    let now = new Date().getTime();
-    let gap = countDate - now;
+  let countDate = new Date("July 30, 2023 00:00:00").getTime();
+  let now = new Date().getTime();
+  let gap = countDate - now;
 
-    // calculating time units
-    let second = 1000;
-    let minute = second * 60;
-    let hour = minute * 60;
-    let day = hour * 24;
+  // calculating time units
+  let second = 1000;
+  let minute = second * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
 
-    // calculate seconds, minutes, hours and days depending on the gap
-    let day_counter = Math.floor(gap / day);
-    let hour_counter = Math.floor((gap % day) / hour);
-    let minute_counter = Math.floor((gap % hour) / minute);
-    let second_counter = Math.floor((gap % minute) / second);
+  // calculate seconds, minutes, hours and days depending on the gap
+  let day_counter = Math.floor(gap / day);
+  let hour_counter = Math.floor((gap % day) / hour);
+  let minute_counter = Math.floor((gap % hour) / minute);
+  let second_counter = Math.floor((gap % minute) / second);
 
-    // appling changes with the help of function
-    applyChanges(second_elements, second_counter);
-    applyChanges(minute_elements, minute_counter);
-    applyChanges(hour_elements, hour_counter);
-    applyChanges(day_elements, day_counter);
-}
+  // appling changes with the help of function
+  applyChanges(second_elements, second_counter);
+  applyChanges(minute_elements, minute_counter);
+  applyChanges(hour_elements, hour_counter);
+  applyChanges(day_elements, day_counter);
+};
 
 function applyChanges(time_unit_elements, time_counter) {
-    time_unit_elements.forEach(element => {
-        element.textContent = time_counter.toString().padStart(2, "0");
-    });
+  time_unit_elements.forEach((element) => {
+    element.textContent = time_counter.toString().padStart(2, "0");
+  });
 }
 
 setInterval(countdown, 1000);
-
-
-
-
 
 // second method
 
@@ -49,15 +45,13 @@ setInterval(countdown, 1000);
 // let mintues_elements = document.querySelectorAll(".minutes");
 // let seconds_elements = document.querySelectorAll(".seconds");
 
-
-
 // function timer() {
 //     // get element values
 //     let days = getTimeUnits(days_elements);
 //     let hours = getTimeUnits(hours_elements);
 //     let minutes = getTimeUnits(mintues_elements);
 //     let seconds = getTimeUnits(seconds_elements);
-    
+
 //     // reduce seconds by 1
 //     seconds--;
 //     numberChange(seconds_elements, seconds);
@@ -71,7 +65,7 @@ setInterval(countdown, 1000);
 //     if (days == 0 && hours == 0 && minutes == 0 && seconds == 0) {
 //         clearInterval(intervalID);
 //     }
-    
+
 // }
 
 // // function for time change
@@ -95,9 +89,7 @@ setInterval(countdown, 1000);
 //     time_unit_element.forEach(e => {
 //         e.textContent = time_unit.toString().padStart(2, "0");
 //     });
-// } 
+// }
 
 // // calls timer function every 1 second
 // const intervalID = setInterval(timer, 1000);
-
-
